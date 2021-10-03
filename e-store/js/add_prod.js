7b15082dcd3dc90774c1c9dedb2947f12d56f3cc
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2021-10-04 00:08:33 +0200

    Delete e-store directory

--- e-store/js/add_prod.js
+++
@@ -1,8 +0,0 @@
-function cancel(){
-  document.getElementsByName('name')[0].value = "";
-  document.getElementsByName('unit_price')[0].value = "";
-  document.getElementsByName('sale')[0].value = "";
-  document.getElementsByName('type')[0].value = "";
-  document.getElementsByName('img')[0].value = "";
-  document.getElementsByName('stock')[0].value = "";
-}
